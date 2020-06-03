33331ea63e0e66db177aad55654fde86286f1cdb
🎉 unzoa <unzoa@deMacBook-Pro.local>
2020-06-03 18:09:50 +0800

    增加方向选择

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-let con = document.querySelector('.con')
+let direction = 'vertical'
 
+let con = document.querySelector('.con')
 let intervalObj = {} // 定时对象
 let moveInterval = 4 // 间隔
-let moveTime = window.innerWidth / 5 * moveInterval // 移动的时间
+let moveItemCount = 5
+let moveTime = (direction === 'vertical' ? con.offsetHeight : window.innerWidth) / moveItemCount * moveInterval // 移动的时间
 
+let autoplayTimer = {}
 /**
  * [slowlyMove 容器 .con 慢慢移动]
  */
 function slowlyMove () {
   intervalObj = setInterval(() => {
-    con.scrollLeft += 1 // 最小整数
+    direction === 'vertical'
+      ? con.scrollTop += 1 // 向上移动
+      : con.scrollLeft += 1 // 向左移动
   }, moveInterval)
 
   setTimeout(() => {
@@ -22,7 +27,7 @@ function add () {
 
   // 创建新增节点，并添加
   let li = document.createElement('li')
-  li.className = 'active'
+  li.className = 'slide-item active ' + direction
   li.style['animation-duration'] = '1s' // moveTime / 1000 + 's'
   li.style.background = '#3399f3'
   conUl.appendChild(li)
@@ -37,7 +42,16 @@ function add () {
 }
 
 function action () {
-  setInterval(() => {
+  autoplayTimer = setInterval(() => {
     add()
   }, 3 * 1000)
+}
+
+function directChange (value) {
+  clearInterval(autoplayTimer)
+  direction = value
+  let items = document.getElementsByClassName('slide-item')
+  for (let item of items) {
+    item.className = 'slide-item ' + value
+  }
 }
